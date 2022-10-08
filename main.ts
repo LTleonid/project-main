2d7a62b9dc414851c85cfabd06fcb87b39f5e312
LT_leonid <76744566+LTleonid@users.noreply.github.com>
2022-10-08 14:14:47 +0700

    Обновить pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,52 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Number2 = receivedNumber
+})
+let ONOFF = 0
+let Number2 = 0
+radio.setGroup(777)
+/**
+ * 1 - вкл
+ * 
+ * 0 - выкл
+ * 
+ * 3 - работает
+ */
 basic.forever(function () {
-	
+    if (Number2 == 3) {
+        music.playTone(932, music.beat(BeatFraction.Breve))
+        basic.clearScreen()
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+        ONOFF = 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(0)
+        Number2 = 0
+        ONOFF = 0
+    }
+    if (ONOFF == 1) {
+        basic.showLeds(`
+            . . . . .
+            # . # . #
+            # . # . #
+            # . # . #
+            . # # # .
+            `)
+    } else if (ONOFF == 0) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    }
 })
